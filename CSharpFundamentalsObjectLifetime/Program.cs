97025a1d3e4ecdc079a0da43af2d68204d667ac5
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-09 07:16:50 -0500

    added example of conflicting method signatures and an overloaded constructor

--- CSharpFundamentalsObjectLifetime/Program.cs
+++ CSharpFundamentalsObjectLifetime/Program.cs
@@ -22,15 +22,47 @@ namespace CSharpFundamentalsObjectLifetime
             myOtherCar = null;
             myCar = null;
 
+            Car myThirdCar = new Car(); //using the default constructor
+            Car myOverloadedCar = new Car("Honda", "Del Sol", 1997, "Black"); //using the overloaded constructor
         }
 
         class Car
         {
+            //these are the props of the object
             public string Make { get; set; }
             public string Model { get; set; }
             public int Year { get; set; }
             public string Color { get; set; }
+            public double OriginalPrice { get; set; }
 
+
+            //this is a constructor. you can use it to set the object to a valid state
+            //as soon as it is created, setting default values for props, etc. 
+            public Car() 
+            {
+                //you could load this from a configuration file, a database, etc.
+                //we are just hardcoding this for the example.
+                this.Make = "Nissan"; //by default, every new Car is a Nissan. 
+            }
+
+            //this is an overloaded method for the constructor. 
+            public Car(string make, string model, int year, string color)
+            {
+                Make = make;
+                Model = model;
+                Year = year;
+                Color = color;
+            }
+
+            //param names don't affect the method signature. the method below will cause an error
+            //because it has the same signature as the overloaded constructor does. 
+            //public Car(string otherMake, string model, int year, string color)
+            //{
+            //    Make = otherMake;
+            //    Model = model;
+            //    Year = year;
+            //    Color = color;
+            //}
         }
     }
 }
