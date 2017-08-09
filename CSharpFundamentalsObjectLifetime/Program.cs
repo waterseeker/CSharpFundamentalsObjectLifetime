560ebc6d4ea5d2a612cbd2bb6a7933a1256c9f8e
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-09 06:49:04 -0500

    added car class

--- CSharpFundamentalsObjectLifetime/Program.cs
+++ CSharpFundamentalsObjectLifetime/Program.cs
@@ -7,5 +7,14 @@ namespace CSharpFundamentalsObjectLifetime
         static void Main(string[] args)
         {
         }
+
+        class Car
+        {
+            public string Make { get; set; }
+            public string Model { get; set; }
+            public int Year { get; set; }
+            public string Color { get; set; }
+
+        }
     }
 }
