1a57368c46a33e16e64358626ad48791748219c8
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-09 07:01:40 -0500

    added some notes about reference types and garbage collection

--- CSharpFundamentalsObjectLifetime/Program.cs
+++ CSharpFundamentalsObjectLifetime/Program.cs
@@ -6,7 +6,22 @@ namespace CSharpFundamentalsObjectLifetime
     {
         static void Main(string[] args)
         {
+            //creates a new instance of a car
+            //and sets the var myCar as a reference to the object's location in memory
             Car myCar = new Car();
+
+            //this just points to the same spot in memory, 
+            //not another instance of a Car. Reference type vs. Value type. 
+            Car myOtherCar = myCar;
+
+            //removes references to that spot in memory
+            //at some point the garbage collection in C#
+            //will check to see if there are any objects in memory
+            //that have no references pointing at them. 
+            //If it finds any objects with no references it will delete them. 
+            myOtherCar = null;
+            myCar = null;
+
         }
 
         class Car
